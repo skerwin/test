194570f602a1626028b584c33e26518148ac7948
摔倒了爬起来再哭 <634339374@qq.com>
2016-02-26 17:53:21 +0800

    444
    ggg

---
+++ CycleScrollView/CycleScrollView/testViewController.swift
@@ -0,0 +1,35 @@
+//
+//  testViewController.swift
+//  CycleScrollView
+//
+//  Created by berchina on 16/2/26.
+//  Copyright © 2016年 ZYJ. All rights reserved.
+//
+
+import UIKit
+
+class testViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
